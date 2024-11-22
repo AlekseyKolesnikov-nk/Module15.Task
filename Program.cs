76975c42e38dfc5a4bdc0d9acdffe544fabd62e0
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-11-22 18:40:20 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Module15.Model;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var classes = new[]
+        {
+           new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+           new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+           new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+        };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        var allStudents = classes.SelectMany(s => s.Students).ToArray();
+        return allStudents;
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
